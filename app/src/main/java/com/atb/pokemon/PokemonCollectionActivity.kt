cab92408bf17d0dab9d12e5a755a7cd9aa95945e
atbee <athibet.pi@gmail.com>
2023-12-02 14:36:30 +0700

    Create a new Pokemon collection activity

---
+++ app/src/main/java/com/atb/pokemon/PokemonCollectionActivity.kt
@@ -0,0 +1,11 @@
+package com.atb.pokemon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PokemonCollectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon_collection)
+    }
+}
